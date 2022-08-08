88da6696bec452aa5ab9636f53b0d23be5fd9228
Alexander Sporn <github@alexsporn.de>
2022-08-08 17:16:11 +0200

    - Add documentation

---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    plugins: [
+        [
+            '@docusaurus/plugin-content-docs',
+            {
+                id: 'inx-dashboard-develop',
+                path: path.resolve(__dirname, 'docs'),
+                routeBasePath: 'inx-dashboard',
+                sidebarPath: path.resolve(__dirname, 'sidebars.js'),
+                editUrl: 'https://github.com/gohornet/inx-indexer/edit/develop/documentation/docs',
+                versions: {
+                    current: {
+                        label: 'Develop',
+                        path: 'develop',
+                        badge: true
+                    },
+                },
+            }
+        ],
+    ],
+    staticDirectories: [path.resolve(__dirname, 'static')],
+};
